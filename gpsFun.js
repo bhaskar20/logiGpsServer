faf9cc68d22cbbb2b0cd1c26fe1caf29ffb8b625
shitalgodara <shital@trumplab.com>
2016-01-19 14:31:12 +0530

    basic server gps data coming testing

--- gpsFun.js
+++ gpsFun.js
@@ -1,17 +1,22 @@
 var gps = require("gpslibgt06n");
 var gpsModel = require('./models/gps');
+
 var options = {
-    'debug': true,
-    'port': 8080,
-    'device_adapter': "GT06"
+    'debug'                 : true,
+    'port'                  : 8080,
+    'device_adapter'        : "GT06"
 }
 
-var server = gps.server(options, function(device, connection) {
-    connection.on("data", function(res) {});
-    connection.on("end", function(data) {
-        console.log("device left" + connection.device.uid + "from server");
+var server = gps.server(options,function(device,connection){
+    connection.on("data",function(res){
+        console.log(res+ "from server");
+    });
+    connection.on("end",function (data) {
+        console.log("device left" + connection.device.uid+ "from server");
     })
-    device.on("login_request", function(uid, data) {
+    device.on("login_request",function (uid,data) {
+        console.log(uid,data + "from server");
+
         var gpsinfo = new gpsModel({
             "gpsId": uid,
             "location": [1.4, 2.34243],
@@ -24,13 +29,17 @@ var server = gps.server(options, function(device, connection) {
 
             console.log('User saved successfully!');
         });
-        console.log(uid, data + "from server");
-        device.login_authorized(true, data);
+        console.log("testing login from server");
+
+
+        device.login_authorized(true,data);
+
+
     });
-    device.on("ping", function(data) {
+    device.on("ping",function (data) {
         var gpsinfo = new gpsModel({
-            "gpsId": connection.device.uid,
-            "location": [1.4, 2.2],
+            "gpsId": uid,
+            "location": [1.4, 2.243],
             "speed": 10.4
         });
 
@@ -40,18 +49,20 @@ var server = gps.server(options, function(device, connection) {
 
             console.log('User saved successfully!');
         });
-        console.log("data from:" + connection.device.uid + "from server");
+        console.log( "from ping from server");
+
+        console.log("data from:"+ connection.device.uid+ "from server");
         console.log(data + "from server");
     });
-    device.on("alarm", function(data, alarmData, fullData) {
+    device.on("alarm",function (data,alarmData,fullData) {
         console.log(data + "from server");
         console.log(alarmData + "from server");
         console.log(fullData + "from server");
     });
-    device.on("status", function(data, fullData) {
-        console.log("data from:" + connection.device.uid + "from server");
-        console.log(data.termInfoContent, voltSignal, gsmSignal + "from server");
-        console.log(fullData + "from server");
+    device.on("status",function (data,fullData) {
+        console.log("data from:"+ connection.device.uid+ "from server");
+        console.log(data + "from server");
+        console.log(fullData+ "from server");
     });
 
 
